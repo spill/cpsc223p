cebd19566b808690bfcdfe102b1ee9bdf7ab48b5
ryan <rtrinh02@DESKTOP-32PH758.localdomain>
2024-03-07 20:19:23 -0800

    part 2 finished

---
+++ Lab-06/223P-Lab-06-02/calculator.py
@@ -0,0 +1,16 @@
+def multiply(x, y):
+    result = x * y
+    print(f"{x} times {y} is {result}")
+
+
+def divide(x, y):
+    result = x / y
+    print(f"{x} divided by {y} is {result}")
+
+if __name__ == "__main__":
+    multiply(2, 1)
+    divide(6, 2)
+        
+        
+        
+
